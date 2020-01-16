2bdf9e5325563f2a4e3bb95edf3bbb20399ba99a
Max Gonzih <gonzih@gmail.com>
2020-01-15 20:05:55 -0500

    Does not work still :(

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,9 @@ use rand::seq::SliceRandom;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::Clamped;
 use wasm_bindgen::JsCast;
-use web_sys::{CanvasRenderingContext2d, HtmlImageElement, ImageData};
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, HtmlImageElement, ImageData};
+
+const DPI: u32 = 72;
 
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
@@ -46,9 +48,11 @@ fn separation(z: f32, mu: f32, e: f32) -> i32 {
     ((1.0 - mu * z) * e * (2.0 - mu * z)).round() as i32
 }
 
+type DepthMap = Vec<Vec<f32>>;
+
 #[allow(clippy::needless_range_loop)]
-fn ctx_to_depth_map(ctx: &CanvasRenderingContext2d, w: u32, h: u32) -> Vec<Vec<u8>> {
-    let mut result = vec![vec![0; w as usize]; h as usize];
+fn ctx_to_depth_map(ctx: &CanvasRenderingContext2d, w: u32, h: u32) -> DepthMap {
+    let mut result: DepthMap = vec![vec![1.0; w as usize]; h as usize];
     let image_data = ctx
         .get_image_data(0.0, 0.0, w as f64, h as f64)
         .expect("Could not get image data")
@@ -57,14 +61,25 @@ fn ctx_to_depth_map(ctx: &CanvasRenderingContext2d, w: u32, h: u32) -> Vec<Vec<u
     for y in 0..(w as usize) {
         let offset = (w * y as u32 * 4) as usize;
         for x in 0..(h as usize) {
-            result[y][x] = image_data[offset + (x as usize * 4)]
+            result[y][x] = 1.0 - image_data[offset + (x as usize * 4)] as f32 / 255.0;
         }
     }
 
     result
 }
 
-pub fn img_to_depth_map(img: &HtmlImageElement, w: u32, h: u32) -> Vec<Vec<u8>> {
+pub fn canvas_to_depth_map(canvas: &HtmlCanvasElement, w: u32, h: u32) -> DepthMap {
+    let ctx = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    ctx_to_depth_map(&ctx, w, h)
+}
+
+pub fn img_to_depth_map(img: &HtmlImageElement, w: u32, h: u32) -> DepthMap {
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document
         .create_element("canvas")
@@ -85,20 +100,39 @@ pub fn img_to_depth_map(img: &HtmlImageElement, w: u32, h: u32) -> Vec<Vec<u8>> 
     ctx.draw_image_with_html_image_element(img, 0.0, 0.0)
         .unwrap();
 
-    // DEBUG CANVAS
-    document.body().unwrap().append_child(&canvas).unwrap();
-
     ctx_to_depth_map(&ctx, w, h)
+}
+
+pub fn reset_canvas(ctx: &CanvasRenderingContext2d, mut pixel_data: Vec<u8>, w: u32, h: u32) {
+    let image_data = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&mut pixel_data), w, h)
+        .expect("Could not create image data");
+
+    ctx.put_image_data(&image_data, 0.0, 0.0)
+        .expect("Could not put image data");
+
+    log!("Finished rendering!");
+}
+
+#[wasm_bindgen]
+pub fn render_canvas(canvas: &HtmlCanvasElement, ctx: &CanvasRenderingContext2d, w: u32, h: u32) {
+    let depth_map = canvas_to_depth_map(canvas, w, h);
+    let pixel_data = generate_pixel_data(depth_map, w, h, DPI);
+    reset_canvas(ctx, pixel_data, w, h);
 }
 
 #[wasm_bindgen]
 pub fn render_img(img: &HtmlImageElement, ctx: &CanvasRenderingContext2d, w: u32, h: u32) {
     let depth_map = img_to_depth_map(img, w, h);
-    render(depth_map, ctx, w, h);
+    let pixel_data = generate_pixel_data(depth_map, w, h, DPI);
+    reset_canvas(ctx, pixel_data, w, h);
 }
 
 #[allow(clippy::needless_range_loop)]
-pub fn render(depth_map: Vec<Vec<u8>>, ctx: &CanvasRenderingContext2d, w: u32, h: u32) {
+/// This is implementation of algorithm from this paper:
+/// https://www2.cs.sfu.ca/CourseCentral/414/li/material/refs/SIRDS-Computer-94.pdf
+/// this also uses this library as a working reference with canvas and img tags
+/// https://github.com/peeinears/MagicEye.js
+pub fn generate_pixel_data(depth_map: DepthMap, w: u32, h: u32, dpi: u32) -> Vec<u8> {
     let colors = vec![
         Color::new(0, 0, 0),
         Color::new(255, 0, 0),
@@ -106,7 +140,6 @@ pub fn render(depth_map: Vec<Vec<u8>>, ctx: &CanvasRenderingContext2d, w: u32, h
         Color::new(0, 0, 255),
         Color::new(255, 255, 255),
     ];
-    let dpi = 72;
     let e = (dpi as f32 * 2.5).round();
     let mu = 1.0 / 3.0;
     // let far = separation(0.0, mu, e);
@@ -115,113 +148,93 @@ pub fn render(depth_map: Vec<Vec<u8>>, ctx: &CanvasRenderingContext2d, w: u32, h
 
     let rng = &mut rand::thread_rng();
 
-    for _ in 0..5 {
-        let color = colors
-            .choose(rng)
-            .expect("Could not get random color")
-            .clone();
-        log!("chose random color: {:?}", color);
-    }
-
-    let mut pixels = vec![vec![colors[0].clone(); height]; width];
+    let mut pixel_data = vec![0_u8; width * height * 4];
 
     for y in 0..height {
+        // color of this given pixel
         let mut pix = vec![colors[0].clone(); width];
-        let mut same = vec![0; width];
+        // points to a pixels on the right that is constrained to be this color
+        let mut same = vec![0_i32; width];
 
         for x in 0..width {
+            // each pixel is initially linked with itself
             same[x] = x as i32;
         }
 
         for x in 0..width {
-            let z = depth_map[x][y] as f32;
+            let z = depth_map[y][x] as f32;
             let sep = separation(z, mu, e);
+            // x values corresponding to the left and right eyes
             let mut left = x as i32 - (sep + (sep & y as i32 & 1)) / 2;
             let mut right = left + sep;
 
+            // do hidden surface removal
             if 0 <= left as i32 && right < width as i32 {
+                // we will check points x-t and x+t
                 let mut t = 1;
                 let mut visible;
 
                 loop {
+                    // z coord of ray at these two points
                     let zt = z + 2.0 * (2.0 - mu * z) * t as f32 / (mu * e);
-                    visible =
-                        (depth_map[x - t][y] as f32) < zt && (depth_map[x + t][y] as f32) < zt;
+                    visible = depth_map[y][x - t] < zt && depth_map[y][x + t] < zt;
                     t += 1;
 
-                    // log!("zt: {}, visible: {}, t: {}", zt, visible, tt);
-                    if !(visible && zt < 1.0) {
+                    if !visible || zt >= 1.0 {
+                        // log!("zt: {}, visible: {}, t: {}", zt, visible, t);
                         break;
                     }
                 }
 
                 if visible {
-                    let k = same[left as usize];
-                    if k != left && k != right {
-                        // log!(
-                        //     "k({}) != left({}) && k({}) != right({}) >>>>> {}",
-                        //     k,
-                        //     left,
-                        //     k,
-                        //     right,
-                        //     k != left && k != right
-                        // );
-                        if k < right {
-                            left = k;
+                    for _ in 0..6 {
+                        let k = same[left as usize];
+                        // juggle pointers until
+                        // either
+                        // same[left] == left or
+                        // same[right] == left
+                        if k != left && k != right {
+                            if k < right {
+                                left = k;
+                            } else {
+                                left = right;
+                                right = k;
+                            }
                         } else {
-                            left = right;
-                            right = k;
+                            break;
                         }
                     }
 
-                    // same[left as usize] = right;
+                    // record the same color
+                    same[left as usize] = right;
                 }
             }
 
-            for x in (width - 1)..0 {
+            for x in (0..width).rev() {
                 // log!("same x: {}", same[x]);
-                // if same[x] == x as i32 {
+                if same[x] == x as i32 {
+                    pix[x] = colors
+                        .choose(rng)
+                        .expect("Could not get random color")
+                        .clone();
                 // THIS IS NEVER EXECUTED
-                pix[x] = colors
-                    .choose(rng)
-                    .expect("Could not get random color")
-                    .clone();
                 // log!("random choise: {:?}", pix[x]);
-                // } else {
-                // pix[x] = pix[same[x] as usize].clone();
-                // }
+                } else {
+                    pix[x] = pix[same[x] as usize].clone();
+                }
 
-                pixels[x][y] = pix[x].clone();
+                let color = &pix[x];
+                let offset = (y * width * 4) + (x * 4);
+                // log!("Set color to {:?}", color);
+                pixel_data[offset] = color.r;
+                pixel_data[offset + 1] = color.g;
+                pixel_data[offset + 2] = color.b;
+                pixel_data[offset + 3] = 255;
             }
         }
         // draw_circle(width/2-far/2, height*19/20);
         // draw_circle(width/2+far/2, height*19/20);
     }
 
-    let mut pixel_data = vec![0_u8; width * height * 4];
-
-    for y in 0..height {
-        for x in 0..width {
-            // let color = &pixels[x][y];
-            let color = &colors.choose(rng).expect("Could not get random color");
-            let offset = (y * width * 4) + (x * 4);
-            // log!("Set color to {:?}", color);
-            pixel_data[offset] = color.r;
-            pixel_data[offset + 1] = color.g;
-            pixel_data[offset + 2] = color.b;
-            pixel_data[offset + 3] = 255;
-        }
-    }
-
-    let image_data = ImageData::new_with_u8_clamped_array_and_sh(
-        Clamped(&mut pixel_data),
-        width as u32,
-        height as u32,
-    )
-    .expect("Could not create image data");
-
-    ctx.put_image_data(&image_data, 0.0, 0.0)
-        .expect("Could not put image data");
-
-    log!("Finished rendering!");
+    pixel_data
 }
