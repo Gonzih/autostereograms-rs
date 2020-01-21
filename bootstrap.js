5456991d11385ce3099c95fa894be36f7ca6dcf2
Max Gonzih via GH Actions <gonzih@gmail.com>
2020-01-21 19:11:16 +0000

    Website updated at Tue Jan 21 19:11:16 UTC 2020

--- bootstrap.js
+++ bootstrap.js
@@ -243,7 +243,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/autostereograms_rs_bg.wasm":"60578d2045e3e985ebc3"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/autostereograms_rs_bg.wasm":"d06e62b41d7264a159cb"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
