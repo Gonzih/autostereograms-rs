7986b909240d82c8aae6be656458f431532efec2
Max Gonzih <gonzih@gmail.com>
2020-01-22 15:26:49 -0500

    Heh

--- www/index.js
+++ www/index.js
@@ -28,9 +28,46 @@ function init_demo_one() {
 function render_circle() {
     let canvas = document.getElementById("source-one");
     let ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath();
-    ctx.arc(canvas.width/2, canvas.height/2, 52, 0, 2 * Math.PI, false);
+    ctx.arc(canvas.width/2, canvas.height/2, 80, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#000';
+    ctx.stroke();
+
+    demo_one_render();
+}
+
+function render_square() {
+    let canvas = document.getElementById("source-one");
+    let ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+    ctx.rect(canvas.width/2-80, canvas.height/2-80, 160, 160);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#000';
+    ctx.stroke();
+
+    demo_one_render();
+}
+
+function render_triangle() {
+    let canvas = document.getElementById("source-one");
+    let ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+    ctx.beginPath();
+    ctx.moveTo(100, 100);
+    ctx.lineTo(100, 300);
+    ctx.lineTo(300, 300);
+    ctx.closePath();
     ctx.fillStyle = 'black';
     ctx.fill();
     ctx.lineWidth = 5;
@@ -46,8 +83,8 @@ function getID(id) {
 
 init_demo_one();
 getID("demo-one-circle").onclick = render_circle;
-getID("demo-one-square").onclick = render_circle;
-getID("demo-one-triangle").onclick = render_circle;
+getID("demo-one-square").onclick = render_square;
+getID("demo-one-triangle").onclick = render_triangle;
 
 function showorigin() {
     getID("demo-one-img-wrapper").style.display = "block";
